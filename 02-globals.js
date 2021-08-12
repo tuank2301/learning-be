8ef0c174217ba9522a5b39eb11b857c3d0846d34
Black Cloud <you@example.com>
2021-08-12 11:15:52 +0700

    8

--- 02-globals.js
+++
@@ -1,5 +0,0 @@
-console.log(__dirname);
-
-setInterval(() => {
-  console.log("Hello World");
-}, 1000);
--- 04-names.js
+++
@@ -1,5 +0,0 @@
-const secret = "SUPER SECRET";
-const john = "John";
-const peter = "Peter";
-
-module.exports = { john, peter };
